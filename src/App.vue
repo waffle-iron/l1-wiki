7efb283a30fa16f499d20737fe040b42e6f973f3
dynamic-gravity <axios350@gmail.com>
2018-03-14 11:04:20 -0400

    set up desired layout

--- src/App.vue
+++ src/App.vue
@@ -29,19 +29,19 @@
       :clipped-left="clipped"
     >
       <v-toolbar-side-icon @click.stop="drawer = !drawer"></v-toolbar-side-icon>
-      <v-btn icon @click.stop="miniVariant = !miniVariant">
+      <!-- <v-btn icon @click.stop="miniVariant = !miniVariant">
         <v-icon v-html="miniVariant ? 'chevron_right' : 'chevron_left'"></v-icon>
-      </v-btn>
-      <v-btn icon @click.stop="clipped = !clipped">
+      </v-btn> -->
+      <!-- <v-btn icon @click.stop="clipped = !clipped">
         <v-icon>web</v-icon>
-      </v-btn>
-      <v-btn icon @click.stop="fixed = !fixed">
+      </v-btn> -->
+      <!-- <v-btn icon @click.stop="fixed = !fixed">
         <v-icon>remove</v-icon>
-      </v-btn>
+      </v-btn> -->
       <v-toolbar-title v-text="title"></v-toolbar-title>
       <v-spacer></v-spacer>
       <v-btn icon @click.stop="rightDrawer = !rightDrawer">
-        <v-icon>menu</v-icon>
+        <v-icon>more_vert</v-icon>
       </v-btn>
     </v-toolbar>
     <v-content>
@@ -55,17 +55,27 @@
       app
     >
       <v-list>
-        <v-list-tile @click="right = !right">
+        <!-- <v-list-tile @click="right = !right">
           <v-list-tile-action>
             <v-icon>compare_arrows</v-icon>
           </v-list-tile-action>
           <v-list-tile-title>Switch drawer (click me)</v-list-tile-title>
+        </v-list-tile> -->
+        <v-list-tile>
+          <v-list-tile-action>
+            <v-list-tile-avatar>
+              <!-- Avatar image goes here -->
+            </v-list-tile-avatar>
+          </v-list-tile-action>
+          <v-list-tile-title>
+            <!-- User name goes here -->
+          </v-list-tile-title>
         </v-list-tile>
       </v-list>
     </v-navigation-drawer>
-    <v-footer :fixed="fixed" app>
+    <!-- <v-footer :fixed="fixed" app>
       <span>&copy; 2017</span>
-    </v-footer>
+    </v-footer> -->
   </v-app>
 </template>
 
@@ -73,7 +83,7 @@
 export default {
   data () {
     return {
-      clipped: false,
+      clipped: true,
       drawer: true,
       fixed: false,
       items: [{
@@ -83,7 +93,7 @@ export default {
       miniVariant: false,
       right: true,
       rightDrawer: false,
-      title: 'Vuetify.js'
+      title: '[un]Official Level1Techs Wiki'
     }
   },
   name: 'App'
